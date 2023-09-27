6e01908e6e749cb7a306b74ce322ef89c9bf546a
Simone Tesini <simozilla006@gmail.com>
2023-09-27 18:07:14 +0200

    refactor: change decoratedHandle to handle

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ import (
 	_ "github.com/joho/godotenv/autoload"
 )
 
-func decoratedHandle(r *mux.Router, path string, f handler.APIFunc, stores db.StoreHolder) {
+func handle(r *mux.Router, path string, f handler.APIFunc, stores db.StoreHolder) {
 	r.HandleFunc("/api"+path, handler.DecorateHTTPFunc(f, stores))
 }
 
@@ -29,18 +29,20 @@ func main() {
 
 	r := mux.NewRouter()
 	// auth
-	decoratedHandle(r, "/unprotected/user/signup", handler.UserSignup, stores)
-	decoratedHandle(r, "/unprotected/user/login", handler.UserLogin, stores)
+	handle(r, "/unprotected/user/signup", handler.UserSignup, stores)
+	handle(r, "/unprotected/user/login", handler.UserLogin, stores)
 
 	// user
-	decoratedHandle(r, "/user/one", handler.OneUser, stores)
-	decoratedHandle(r, "/user/me", handler.UserMe, stores)
+	handle(r, "/user/one", handler.OneUser, stores)
+	handle(r, "/user/me", handler.UserMe, stores)
 
 	// org
-	decoratedHandle(r, "/admin/org/new", handler.OrgNew, stores)
-	decoratedHandle(r, "/org/@{tag}", handler.Org, stores)
-	decoratedHandle(r, "/org/@{tag}/meta", handler.OrgMeta, stores)
-	decoratedHandle(r, "/org/@{tag}/adventure/all", handler.OrgAdventuresAll, stores)
+	handle(r, "/admin/org/new", handler.OrgNew, stores)
+	handle(r, "/org/@{tag}", handler.Org, stores)
+	handle(r, "/org/@{tag}/meta", handler.OrgMeta, stores)
+
+	// org adventures
+	handle(r, "/org/@{tag}/adventure/all", handler.OrgAdventuresAll, stores)
 
 	fmt.Println("Server running on http://localhost:8080")
 	http.ListenAndServe(":8080", r)
