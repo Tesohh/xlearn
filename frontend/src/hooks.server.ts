934d7285df6f72e3ab657c6d7d59cce1695f5e1d
Mat12143 <skymatti12@gmail.com>
2023-10-07 16:28:46 +0200

    feat: auth in hooks

--- frontend/src/hooks.server.ts
+++ frontend/src/hooks.server.ts
@@ -1,11 +1,21 @@
+import { backendUrl } from '$lib/const';
+
 export const handle = async ({ event, resolve }) => {
 	const cookie = event.cookies.get('auth');
 
 	if (cookie) {
-		event.locals.user = {
-			username: 'test',
-			level: 1
-		};
+		const resp = await fetch(`${backendUrl}/api/user/me`, {
+			method: 'GET',
+			headers: {
+				Cookie: `jwt=${cookie}`
+			}
+		});
+
+		if (!resp.ok) event.locals.user = null;
+		else {
+			let jsonResp = await resp.json();
+			console.log(jsonResp);
+		}
 	} else {
 		event.locals.user = null;
 	}
--- frontend/src/lib/const.ts
+++ frontend/src/lib/const.ts
@@ -1,1 +1,1 @@
-export const backendUrl = '127.0.0.1:8080';
+export const backendUrl = 'http://localhost:8080';
