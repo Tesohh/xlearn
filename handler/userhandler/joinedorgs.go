c39971aa9eea5da604d6fd26c79ebebd9554df17
Simone Tesini <simozilla006@gmail.com>
2023-11-03 18:11:23 +0100

    feat: add special case on joined orgs for admins where they can see every org

--- handler/userhandler/joinedorgs.go
+++ handler/userhandler/joinedorgs.go
@@ -1,8 +1,10 @@
 package userhandler
 
 import (
+	"fmt"
 	"net/http"
 
+	"github.com/Tesohh/xlearn/data"
 	"github.com/Tesohh/xlearn/db"
 	"github.com/Tesohh/xlearn/handler"
 )
@@ -13,20 +15,32 @@ func JoinedOrgs(w http.ResponseWriter, r *http.Request, stores db.StoreHolder) e
 		return err
 	}
 
-	orgs, err := db.Populate(user.JoinedOrgs, stores.Orgs)
-	if err != nil {
-		return err
-	}
+	var orgs []data.Org
 
-	allOrgs, err := stores.Orgs.Many(db.Query{})
-	if err != nil {
-		return err
-	}
-
-	for _, v := range allOrgs {
-		if v.IsUnprotected {
-			orgs = append(orgs, v)
+	if user.Role < data.RoleAdmin { // not admin
+		fmt.Println("ZESTFEST", user.Role)
+		orgs, err = db.Populate(user.JoinedOrgs, stores.Orgs)
+		if err != nil {
+			return err
 		}
+
+		allOrgs, err := stores.Orgs.Many(db.Query{})
+		if err != nil {
+			return err
+		}
+
+		for _, v := range allOrgs {
+			if v.IsUnprotected {
+				orgs = append(orgs, v)
+			}
+		}
+	} else { // admin
+		fmt.Println("BRODIE")
+		orgs, err = stores.Orgs.Many(db.Query{})
+		if err != nil {
+			return err
+		}
+
 	}
 
 	handler.WriteJSON(w, 200, orgs)
