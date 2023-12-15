f2b132ff70994651d3a45472bd51e5afd4b8847d
Simone Tesini <simozilla006@gmail.com>
2023-12-15 17:51:45 +0100

    feat: make admins see everything in joined orgs tags

--- handler/userhandler/joinedorgstags.go
+++ handler/userhandler/joinedorgstags.go
@@ -3,6 +3,7 @@ package userhandler
 import (
 	"net/http"
 
+	"github.com/Tesohh/xlearn/data"
 	"github.com/Tesohh/xlearn/db"
 	"github.com/Tesohh/xlearn/handler"
 )
@@ -15,19 +16,30 @@ func JoinedOrgsTags(w http.ResponseWriter, r *http.Request, stores db.StoreHolde
 
 	orgs := user.JoinedOrgs
 
-	allOrgs, err := stores.Orgs.Many(db.Query{})
-	if err != nil {
-		return err
-	}
+	if user.Role < data.RoleAdmin {
+		allOrgs, err := stores.Orgs.Many(db.Query{})
+		if err != nil {
+			return err
+		}
 
-	for _, v := range allOrgs {
-		if v.IsUnprotected {
+		for _, v := range allOrgs {
+			if v.IsUnprotected {
+				orgs = append(orgs, v.Tag)
+			}
+		}
+
+		if orgs == nil {
+			orgs = make([]string, 0)
+		}
+	} else {
+		allOrgs, err := stores.Orgs.Many(db.Query{})
+		if err != nil {
+			return nil
+		}
+
+		for _, v := range allOrgs {
 			orgs = append(orgs, v.Tag)
 		}
-	}
-
-	if orgs == nil {
-		orgs = make([]string, 0)
 	}
 
 	handler.WriteJSON(w, 200, orgs)
